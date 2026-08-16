e14b4f0b56dd0311578a2e8ea2224084c70934bd
anaherrera4-ops <ana.herrera4@udea.edu.co>
2026-08-15 20:45:22 -0500

    Add files via upload

---
+++ EJERCICIO3.py
@@ -0,0 +1,15 @@
+nota1 = float(input("Ingrese nota 1: "))
+nota2 = float(input("Ingrese nota 2: "))
+nota3 = float(input("Ingrese nota 3: "))
+nota4 = float(input("Ingrese nota 4: "))
+nota5 = float(input("Ingrese nota 5: "))
+
+promedio = (nota1 * 0.30) + (nota2 * 0.15) + (nota3 * 0.15) + (nota4 * 0.20) + (nota5 * 0.20)
+
+print(f"El promedio ponderado es: {promedio:.2f}") #lo que está adentro de las llaves no es texto es una variable, 2f dos décimales
+
+
+
+
+
+
