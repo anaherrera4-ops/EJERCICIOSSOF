36105c4523d2bc8b0de5265be6a5039bb145cb6f
anaherrera4-ops <ana.herrera4@udea.edu.co>
2026-08-15 20:42:57 -0500

    Add files via upload

---
+++ ACTIVIDAD2.py
@@ -0,0 +1,10 @@
+#EJERCICIO 2:
+sueldouno=float(input("ingrese sueldo uno"))
+sueldodos=float(input("ingrese sueldo dos"))
+sueldotres=float(input("ingrese sueldo tres"))
+nuevosueldouno= sueldouno*1.10
+nuevosueldodos= sueldodos*1.12
+nuevosueldotres= sueldotres*1.15
+print("el sueldo nuevo uno es de:", nuevosueldouno)
+print("el sueldo nuevo dos es de:", nuevosueldodos)
+print("el sueldo nuevo tres es de:", nuevosueldotres)
