569c8d2ef84c3e2293e224ca23d6b803f552361d
anaherrera4-ops <ana.herrera4@udea.edu.co>
2026-08-15 20:46:44 -0500

    ÁREA Y PERÍMETRO DE UN CIRCULO

---
+++ EJERCICIO_4.py
@@ -0,0 +1,6 @@
+radio=float(input("ingrese el radio de la circunferencia: "))
+pi=3.1416
+area= pi*(radio**2)
+perímetro= 2* pi *radio
+print(f"el area de la circunferencia es: {area:.2f} ")                  
+print(f"el perímetro de la circunferencia es: {perímetro:.2f} ")
