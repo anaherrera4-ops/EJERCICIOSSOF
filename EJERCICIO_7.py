ab7ee276afbd7177edaf8fe50d9c5b6433879fb1
anaherrera4-ops <ana.herrera4@udea.edu.co>
2026-08-17 13:51:11 -0500

    Add files via upload
    GRADOS CELSIUS A GRADOS KELVIN Y FAHRENHEIT

---
+++ EJERCICIO_7.py
@@ -0,0 +1,6 @@
+gradoscelsius=float(input("ingrese la cantidad en grados celsius: "))
+kelvin=gradoscelsius + 273.15
+fahrenheit=(kelvin-273.15)*1.8+32
+print(f"{gradoscelsius} grados celsius equivalen a: {kelvin} grados kelvin y {fahrenheit} fahrenheit")
+
+
