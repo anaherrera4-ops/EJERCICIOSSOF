28b83222a73d2c5fec2ae2d6f972e2c33c7e2516
anaherrera4-ops <ana.herrera4@udea.edu.co>
2026-08-17 14:11:11 -0500

    Add files via upload

---
+++ EJERCICIO_1_CONDICIONALES.py
@@ -0,0 +1,8 @@
+A = float(input("Ingrese el valor de A: "))
+B = float(input("Ingrese el valor de B: "))
+if A > B:
+    print("A es mayor que B")
+elif A < B:#elif: si no se cumple lo de arriba se cumple esto
+    print("A es menor que B")
+else:#si no se cumple nada de lo anterior
+    print("A es igual a B")
